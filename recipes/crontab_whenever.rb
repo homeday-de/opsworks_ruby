a755dfa7bd1e2b24365d4f06e97d9e863a93f0cd
Yuri Veremeyenko <yuri.veremeyenko@gmail.com>
2017-02-24 13:06:45 +0100

    don’t call whenever if it is not installed

--- recipes/crontab_whenever.rb
+++ recipes/crontab_whenever.rb
@@ -6,7 +6,15 @@ every_enabled_application do |application|
   execute 'update whenever schedule' do
     cwd File.join(deploy_dir(application), 'current')
     user 'deploy'
-    command 'bundle exec whenever --clear-crontab && bundle exec whenever --write-crontab'
+    command <<-CMD
+/usr/local/bin/bundle show whenever;
+if [ $? -eq 0 ]
+then
+  bundle exec whenever --clear-crontab && bundle exec whenever --write-crontab
+else
+  echo "whenever not installed, skipping"
+fi
+    CMD
     action :run
   end
 end
