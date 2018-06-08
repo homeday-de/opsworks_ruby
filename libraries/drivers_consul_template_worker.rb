ee74688632e26b9fd86f51e2e130441ad5a6871e
Yuri Veremeyenko <yuri.veremeyenko@gmail.com>
2018-06-08 15:27:08 +0200

    renew vault tokens manually

--- libraries/drivers_consul_template_worker.rb
+++ libraries/drivers_consul_template_worker.rb
@@ -1,3 +1,5 @@
+require 'time'
+
 module Drivers
   module ConsulTemplate
     class Worker < Drivers::Base
@@ -17,6 +19,8 @@ module Drivers
       def deploy_before_migrate
         return unless vault_enabled?
         template_str = "#{consul_template_secret_template_path}:#{consul_template_secret_destination_path}"
+        # check if vault token has expired and request a new one if needed
+        update_consul_template_config if vault_token_expired?
         consul_template_cmd = <<-TPL
 consul-template -config=#{consul_template_config_path} -template "#{template_str}" -retry 30s -once 2>&1
 TPL
@@ -36,8 +40,24 @@ TPL
 
       def configure
         return unless vault_enabled?
+        update_consul_template_config
+      end
+
+      def update_consul_template_config
         authenticate_to_vault
         create_consul_template_config
+      end
+
+      def vault_token_expired?
+        vault_token = read_vault_token_from_file
+        vault_api_url = vault_attrs.fetch("vault_url")
+        curl_cmd =
+<<-EOH
+curl --header "X-Vault-Token: #{vault_token}" #{vault_api_url}/v1/auth/token/lookup-self | ruby -e 'require "json"; puts JSON.parse(ARGF.read)["data"]["expire_time"]'
+EOH
+        curl_output = shell_out(curl_cmd)
+        expiration_time_utc = Time.iso8601(curl_output.stdout.strip)
+        Time.now.utc > expiration_time_utc + 20 * 60 # if the token expires within the next 20 minutes, consider it expired
       end
 
       def validate_app_engine
@@ -55,8 +75,11 @@ TPL
 
       attr_reader :vault_token, :vault_url
 
+      def vault_attrs
+        context.node['deploy'][app_shortname]['vault']
+      end
+
       def authenticate_to_vault
-        vault_attrs = context.node['deploy'][app_shortname]['vault']
         @vault_url = vault_attrs.fetch("vault_url")
         app_id = vault_attrs.fetch('app_id')
         user_id = vault_attrs.fetch('user_id')
@@ -67,6 +90,16 @@ EOH
         curl_output = shell_out(curl_cmd)
         @vault_token = curl_output.stdout.strip.gsub(/\A"|"\Z/, '')
         context.node.default['deploy'][app_shortname]['vault']['vault_token'] = @vault_token
+        # write the vault token to a file too
+        File.write(vault_token_file_path) { |f| f.write(@vault_token) }
+      end
+
+      def vault_token_file_path
+        '~/.vault-token'
+      end
+
+      def read_vault_token_from_file
+        File.read(vault_token).chomp
       end
 
       def create_consul_template_config
